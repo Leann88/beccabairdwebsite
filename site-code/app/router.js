e688fbe56013ba8d4ac23cc218bf1be6eda4f98d
Leann Labelle <leann.88.labelle@gmail.com>
2017-08-13 12:50:07 -0400

    generation of pages

--- site-code/app/router.js
+++ site-code/app/router.js
@@ -12,6 +12,11 @@ Router.map(function() {
   });
   this.route('Home');
   this.route('choleric-vision');
+  this.route('portfolio-index');
+  this.route('by-the-numbers');
+  this.route('project-oleum');
+  this.route('scavenger-app-prototype');
+  this.route('sushi-master');
 });
 
 export default Router;
---
+++ site-code/tests/unit/routes/project-oleum-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:project-oleum', 'Unit | Route | project oleum', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
