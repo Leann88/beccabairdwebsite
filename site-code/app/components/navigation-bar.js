3f5b7bbd1fbbac14ac5949b1a95c7fa696d8d3a4
Leann Labelle <leann.88.labelle@gmail.com>
2017-08-21 21:20:45 -0400

    fixing toggling of menu

--- site-code/app/components/navigation-bar.js
+++ site-code/app/components/navigation-bar.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    click(event) {
+    this._super(...arguments);
+    if(Ember.$(event.target).hasClass('nav-link')) {
+      this.$().find('.collapse').collapse('hide');
+    }
+  },
 });
